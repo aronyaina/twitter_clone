b673593f24adcbb0b59b5ba5d770073c3daa020a
aronyaina <ravaronyaina@gmail.com>
2023-03-09 14:56:21 +0300

    Post and profile test

--- components/Feed.js
+++ components/Feed.js
@@ -1,16 +1,43 @@
 import { SparklesIcon } from "@heroicons/react/solid";
 import Input from "./Input";
+import Post from "./Post";
 
 export default function Feed() {
+  const posts = [
+    {
+      id: "1",
+      name: "Aro",
+      username: "Ny Aina",
+      userImage: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
+      image:
+        "https://images.unsplash.com/photo-1678305346922-de3539a25bbd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+      text: "Nice view !",
+      timestamp: "2 hours ago",
+    },
+    {
+      id: "2",
+      name: "Mialy",
+      username: "Soa",
+      userImage: "https://cdn-icons-png.flaticon.com/512/3220/3220315.png",
+      image:
+        "https://images.unsplash.com/photo-1677021048526-4fcbb33abf4b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80",
+      text: "Night city !",
+      timestamp: "1 hours ago",
+    },
+  ];
+
   return (
     <div className="xl:ml-[370px] border-l border-r border-gray-200 xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
       <div className="flex py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
-        <h2 className="text-lg sm:text-xl font-bold cursor-pointer">Home</h2>
+        <h2 className="text-lg sm:text-xl font-bold cursor-pointer"> Home </h2>{" "}
         <div className="hoverEffect flex items-center justify-center px-0 ml-auto w-9 h-9  ">
           <SparklesIcon className="h-5" />
-        </div>
-      </div>
-      <Input />
+        </div>{" "}
+      </div>{" "}
+      <Input />{" "}
+      {posts.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}{" "}
     </div>
   );
 }
---
+++ components/Post.js
@@ -0,0 +1,59 @@
+import {
+  ChartBarIcon,
+  ChatIcon,
+  DotsHorizontalIcon,
+  HeartIcon,
+  ShareIcon,
+  TrashIcon,
+} from "@heroicons/react/solid";
+
+export default function Post({ post }) {
+  return (
+    <div className="flex p-3 cursor-pointer border-b border-gray-200">
+      {/* user image */}
+      <img
+        src={post.userImage}
+        alt="user-image"
+        className="h-11 w-11 rounded-full mr-4"
+      />
+      {/* rigth side */}
+      <div className="">
+        {/* header */}
+        <div className="flex items-center justify-between">
+          {/* user info */}
+          <div className="flex items-center space-x-1 whitespace-nowrap">
+            <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline ">
+              {post.name}
+            </h4>
+            <span className="text-sm sm:text-[15px]">@{post.username}</span>
+            <span className="text-sm sm:text-[15px] hover:underline">
+              - {post.timestamp}
+            </span>
+          </div>
+          {/* icon */}
+          <DotsHorizontalIcon className="h-10 hoverEffect w-10 hover:bg-sky-100 hover:text-sky-500 p-2" />
+        </div>
+
+        {/* post text*/}
+        <p className="text-gray-800 text-[15px sm:text-[16px] ] mb-2">
+          {post.text}
+        </p>
+        {/* post image */}
+        <img
+          className="rounded-2xl mr-2 h-[512px] w-[512px]"
+          src={post.image}
+          alt="post"
+        />
+
+        <div className="flex items-center justify-between mt-2 text-gray-500">
+          {/* icons */}
+          <ChatIcon className="h-9  w-9 hoverEffect p-1 hover:text-sky-500 hover:bg-sky-100" />
+          <TrashIcon className="h-9  hoverEffect p-1 hover:text-red-500 hover:bg-sky-100" />
+          <HeartIcon className="h-9  hoverEffect p-1 hover:text-red-500 hover:bg-sky-100" />
+          <ShareIcon className="h-9 hoverEffect p-1 hover:text-sky-500 hover:bg-sky-100" />
+          <ChartBarIcon className="h-9 hoverEffect p-1 hover:text-sky-500 hover:bg-sky-100" />
+        </div>
+      </div>
+    </div>
+  );
+}
